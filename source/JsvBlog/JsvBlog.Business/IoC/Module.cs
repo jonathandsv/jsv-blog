614cd6492873a7128399abf5dde694ab07412972
Jonathan Vieira <jonathansouza59@gmail.br>
2023-03-10 22:55:58 -0300

    fix: add others class libs to architeture.

---
+++ source/JsvBlog/JsvBlog.Business/IoC/Module.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsvBlog.Business.IoC
+{
+    public static class Module
+    {
+        public static Dictionary<Type, Type> GetType()
+        {
+            var dic = new Dictionary<Type, Type>();
+
+            dic.Add(typeof(Interfaces.IBlogService), typeof(Interfaces.IBlogService));
+            return dic;
+        }
+    }
+}
---
+++ source/JsvBlog/JsvBlog.Business/Services/BlogService.cs
@@ -0,0 +1,13 @@
+﻿using JsvBlog.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsvBlog.Business.Services
+{
+    public class BlogService : IBlogService
+    {
+    }
+}
